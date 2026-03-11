bc067ba1f7a1029858daf1b6b13a5d244178e5ea
Versus1478 <svider.vladyslav@gmail.com>
2026-03-11 19:16:32 +0100

    Small fix

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,11 @@ use App\Http\Controllers\NoteController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('notes', NoteController::class);
-
 Route::get('notes/stats/status', [NoteController::class, 'statsByStatus']);
-
+Route::get('notes/pinned', [NoteController::class, 'pinned']);
+Route::get('notes-actions/search', [NoteController::class, 'search']);
 Route::patch('notes/actions/archive-old-drafts', [NoteController::class, 'archiveOldDrafts']);
-
 Route::get('users/{user}/notes', [NoteController::class, 'userNotesWithCategories']);
 
-Route::get('notes-actions/search', [NoteController::class, 'search']);
-
+Route::apiResource('notes', NoteController::class);
 Route::apiResource('categories', CategoryController::class);
-
-Route::get('notes/pinned', [NoteController::class, 'pinned']);
